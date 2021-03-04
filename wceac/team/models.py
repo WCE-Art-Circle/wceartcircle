9364cf39685bcc0b0597cb71fcddcfa7d63da2b5
Vaishnavi Mahajan <mvaishnavi411@gmail.com>
2021-03-04 16:22:58 +0530

    member model

---
+++ wceac/team/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class member(models.Model):
+    first_name=models.CharField(max_length=50)
+    last_name=models.CharField(max_length=50)
+    board=models.CharField(max_length=50)       #main, asst, member
+    position=models.CharField(max_length=50)    #event dir, treasure, etc
+    team=models.CharField(max_length=50)        #dance, band, etc
+    photo=models.ImageField()                   
+    description=models.CharField(max_length=200,blank=True)#hobbies, beliefs
+    insta=models.URLField(blank=True)           #link to insta account
+    artwork=models.CharField(max_length=100,blank=True)#blogs, art channel, etc
+    artworklink=models.URLField(blank=True)     #link to above artwork
+    valid=models.BooleanField(default=False,blank=True)#currently member or not
