394e2b2dc7acfc8f61bef77dadd355c3c1ed47e8
Vaishnavi Mahajan <mvaishnavi411@gmail.com>
2021-03-28 19:07:08 +0530

    admin

--- team/models.py
+++ team/models.py
@@ -13,3 +13,7 @@ class member(models.Model):
     artwork=models.CharField(max_length=100,blank=True)#blogs, art channel, etc
     artworklink=models.URLField(blank=True)     #link to above artwork
     valid=models.BooleanField(default=False,blank=True)#currently member or not
+
+    def __str__(self):
+        return self.first_name+self.last_name
+    
--- wceac/urls.py
+++ wceac/urls.py
@@ -19,8 +19,8 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    #path('aapconvincehogayeyameinaurbolu/', admin.site.urls),
-    path('admin/', admin.site.urls),
+    path('aapconvincehogayeyameinaurbolu/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('', views.landing.as_view(),name='landing'),
     path('team/', include('team.urls')),
 ]
