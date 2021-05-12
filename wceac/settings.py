f4aa94bb766e5d8d4067bdc2a8ca92cf59456963
Vaishnavi Mahajan <mvaishnavi411@gmail.com>
2021-05-12 15:20:00 +0530

    heroku postgres

--- wceac/settings.py
+++ wceac/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -79,12 +81,21 @@ WSGI_APPLICATION = 'wceac.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    "default": {
+#       "ENGINE": "django.db.backends.postgresql_psycopg2",
+#    }
+#}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+db_from_env=dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
